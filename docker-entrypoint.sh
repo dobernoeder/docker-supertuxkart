1a13ef896028be4ecff773d18e25b5ba1b356ab6
David Obernöder <david@obernoeder.net>
2021-05-16 14:36:38 +0200

    Edited Docker-Script for Use with Kubernetes and added Server-Password as Parameter

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 set -e
 
+/bin/cp /stk/server_config.xml /stk/current_server_config.xml
+
+ADDITIONAL_PARAMETERS=""
+
+if [ -f /stk/USERNAME ]
+then
+  USERNAME=$(cat /stk/USERNAME)
+fi
+
+if [ -f /stk/PASSWORD ]
+then
+  PASSWORD=$(cat /stk/PASSWORD)
+fi
+
+if [ -f /stk/SERVER_PASSWORD ]
+then
+  SERVER_PASSWORD=$(cat /stk/SERVER_PASSWORD)
+fi
+
+
+
 # Log in with username and password if given
 if [ -n ${USERNAME} -a -n ${PASSWORD} ]
 then
-    supertuxkart --init-user --login=${USERNAME} --password=${PASSWORD}
+    ADDITIONAL_PARAMETERS=" --init-user --login=${USERNAME} --password=${PASSWORD}"
 fi
 
 # Start the server
-supertuxkart --server-config=server_config.xml
+if [ -n ${SERVER_PASSWORD} ]
+then
+    ADDITIONAL_PARAMETERS=" --server-password=${SERVER_PASSWORD}"
+fi
+
+supertuxkart --server-config=current_server_config.xml ${ADDITIONAL_PARAMETERS}
